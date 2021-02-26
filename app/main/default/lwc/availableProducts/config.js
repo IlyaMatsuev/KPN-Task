5e7869884d54510f04a37b934460a9af7cb57b92
IlyaMatsuev <matsuev2000@mail.ru>
2021-02-26 03:08:13 +0300

    Configure prettier

--- app/main/default/lwc/availableProducts/config.js
+++ app/main/default/lwc/availableProducts/config.js
@@ -3,19 +3,19 @@
  */
 
 const columns = [
-  { label: "Product name", fieldName: "name", type: "text", sortable: true },
-  {
-    label: "List Price",
-    fieldName: "listPrice",
-    type: "number",
-    sortable: true
-  }
+    { label: 'Product name', fieldName: 'name', type: 'text', sortable: true },
+    {
+        label: 'List Price',
+        fieldName: 'listPrice',
+        type: 'number',
+        sortable: true
+    }
 ];
 
 const sorting = {
-  sortBy: "listPrice",
-  sortByPrioritized: "attachedToOrder",
-  sortDirection: "desc"
+    sortBy: 'listPrice',
+    sortByPrioritized: 'attachedToOrder',
+    sortDirection: 'desc'
 };
 
 export { columns, sorting };
